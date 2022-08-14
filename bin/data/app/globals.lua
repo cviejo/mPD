60324e7b39db55342636549312b70d8234b57ed7
cviejo <carlos.viejo.muros@gmail.com>
2022-08-14 12:55:15 +0200

    added time utils

--- bin/data/app/globals.lua
+++ bin/data/app/globals.lua
@@ -1,6 +1,7 @@
 ---@diagnostic disable: lowercase-global
 local logging = require('utils.logging')
 local inspect = require('libs.inspect')
+local time = require('utils.time')
 
 _G.target = 'desktop'
 _G.dpi = not _G.mpd and 1 or mpd.getDPI()
@@ -12,6 +13,7 @@ _G.glm = glm
 _G.inspect = inspect
 _G.logging = logging
 _G.log = logging.verbose
+_G.setTimeout = time.setTimeout
 _G.TODO = function(msg)
 	print(logging.red('TODO'), msg)
 end
---
+++ bin/data/app/utils/time.lua
@@ -0,0 +1,30 @@
+local F = require('utils.functional')
+
+local M = {}
+
+local timeouts = {}
+
+M.elapsed = of.getElapsedTimeMillis
+
+M.update = function()
+	if #timeouts > 0 then
+		F.forEach(function(x)
+			if M.elapsed() - x.start > x.timeout then
+				x.callback()
+				M.clearTimeout(x)
+			end
+		end, timeouts)
+	end
+end
+
+M.setTimeout = function(callback, timeout)
+	F.push({callback = callback, timeout = timeout, start = M.elapsed()}, timeouts)
+end
+
+M.clearTimeout = function(timeout)
+	timeouts = F.reject(function(x)
+		return x == timeout
+	end, timeouts)
+end
+
+return M
